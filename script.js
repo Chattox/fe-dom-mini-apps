b459fc4a68fc97ab7f3c1750848681df78eecf1e
Chattox <conradlineker@gmail.com>
2020-03-02 15:01:55 +0000

    Added event handling for click, hover, and mouse out

--- script.js
+++ script.js
@@ -1,3 +1,36 @@
-const list = document.querySelector('li');
+const list = [...document.querySelectorAll('li')];
 
-list.addEventListener('click', () => {});
+console.log(list);
+
+const onClick = event => {
+  console.log(event.target);
+  if (!event.target.classList.contains('done')) {
+    event.target.classList.remove('hover');
+    event.target.classList.add('done');
+    console.log(event.target.innerText);
+    event.target.innerText += ' ✅';
+    console.log(event.target.innerText);
+  } else {
+    event.target.classList.remove('done');
+    event.target.innerText = event.target.innerText.replace(' ✅', '');
+  }
+  // console.log(list.classList);
+};
+
+const onHover = event => {
+  if (!event.target.classList.contains('done')) {
+    event.target.classList.add('hover');
+  }
+};
+
+const onOut = event => {
+  if (!event.target.classList.contains('done')) {
+    event.target.classList.remove('hover');
+  }
+};
+
+list.forEach(el => {
+  el.addEventListener('click', onClick);
+  el.addEventListener('pointerover', onHover);
+  el.addEventListener('pointerout', onOut);
+});
